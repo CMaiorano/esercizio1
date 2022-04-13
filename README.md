a73cafbeb9fc6f7e78507b1b17fc064dc83307ab
CMaiorano <carmine.maiorano@gft.com>
2022-04-13 11:55:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# es6-ts-hello-oqwvf7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/es6-ts-hello-oqwvf7)
---
+++ main.ts
@@ -0,0 +1,64 @@
+/**
+ *
+ * ESERCIZIO 1
+ *
+ * Rimpiazza il tipo any con un custom type
+ *
+ * Dovrai creare un nuovo type Member che rappresenti la struttura dell'oggetto
+ *
+ * TIP: puoi utilizzare interface, type o class
+ */
+
+const member: any = {
+  url: 'www.fabiobiondi.io',
+  author: 'Fabio Biondi',
+};
+
+console.log(member)
+member.url = 'asdad'
+console.log(member)
+
+class miaClasse {
+anagrafica : string;
+url: string;
+constructor(nominativo: string, theUrl: string) {
+  this.anagrafica = nominativo;
+  this.url = theUrl;
+}
+}
+
+const elemento = new miaClasse('Fabio Biondi','www.fabiobiondi.io')
+console.log(elemento)
+elemento.anagrafica="minni"
+console.log(elemento)
+elemento.anagrafica = 'Fabio Biondi'
+
+/**
+ * ESERCIZIO 2
+ *
+ * Visualizza la seguente stringa utilizzando i template literals (`string`)
+ *
+ * GOAL
+ *
+ * Fabio Biondi - www.fabiobiondi.io
+ */
+
+/*
+console.log(`
+        Hi 
+            ${name} 
+            ${surname} 
+            (${age})`);
+*/
+
+const star = '*'
+const scopo = 'GOAL'
+
+export const output = `
+            ${star}
+            ${star} ${scopo}
+            ${star}
+            ${elemento.anagrafica} - ${elemento.url}`;
+
+console.log(output);
+document.querySelector('#output').innerHTML = output;
